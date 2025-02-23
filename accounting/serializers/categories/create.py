321c6debdfc8704fe35dfcf82d7c5e49f3507150
Eldos <eldos.baktybekov@gmail.com>
2025-02-23 14:20:44 +0600

    :pencil2: Fix typo

--- accounting/serializers/categories/create.py
+++ accounting/serializers/categories/create.py
@@ -6,7 +6,7 @@ from accounting.models.categories import Category
 class CategoryCreateInputSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=64, min_length=1)
     parent_id = serializers.IntegerField(allow_null=True, default=None)
-    type = serializers.ChoiceField(choices=Category.type.choices)
+    type = serializers.ChoiceField(choices=Category.Type.choices)
 
 
 class CategoryCreateOutputSerializer(serializers.Serializer):
@@ -14,6 +14,6 @@ class CategoryCreateOutputSerializer(serializers.Serializer):
     user_id = serializers.IntegerField()
     name = serializers.CharField()
     parent_id = serializers.IntegerField(allow_null=True)
-    type = serializers.ChoiceField(choices=Category.type.choices)
+    type = serializers.ChoiceField(choices=Category.Type.choices)
     updated_at = serializers.DateTimeField()
     created_at = serializers.DateTimeField()
