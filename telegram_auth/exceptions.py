8c7e1a9cbb07d37d205b9f7f85d9322ca8f3d111
Eldos <eldos.baktybekov@gmail.com>
2025-03-27 20:42:08 +0600

    :construction: Add `UserNotFoundError` exception for handling missing users

--- telegram_auth/exceptions.py
+++ telegram_auth/exceptions.py
@@ -40,3 +40,9 @@ class TelegramMiniAppInitDataRequiredError(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_code = 'telegram_mini_app_init_data_required'
     default_detail = _('Telegram Mini App init data is required')
+
+
+class UserNotFoundError(APIException):
+    status_code = status.HTTP_404_NOT_FOUND
+    default_code = 'user_not_found'
+    default_detail = _('User not found')
