9794caffa3079bacfb9f2d9b904ca9a827cfefc1
Eldos <eldos.baktybekov@gmail.com>
2025-03-27 20:42:33 +0600

    :sparkles: Add cookie-based token refresh functionality and test API for Telegram authentication

--- telegram_auth/urls.py
+++ telegram_auth/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.urls import path
 from .views import TelegramAuthView
 
@@ -5,6 +6,16 @@ from .views import TelegramAuthView
 urlpatterns = [
     path(
         'token/', TelegramAuthView.as_view(),
-        name='token_obtain_telegram'
+        name='token_obtain_telegram',
     ),
 ]
+
+if settings.DEBUG:
+    from .views import TelegramAuthTestApi
+
+    urlpatterns.append(
+        path(
+            'test-token/', TelegramAuthTestApi.as_view(),
+            name='test_telegram_auth',
+        )
+    )
--- telegram_auth/views.py
+++ telegram_auth/views.py
@@ -1,9 +1,45 @@
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from rest_framework import status
+from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.exceptions import InvalidToken
+from rest_framework_simplejwt.serializers import TokenRefreshSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from telegram_auth.services import TelegramAuthenticateInteractor
+
+
+User = get_user_model()
+
+
+class CookieTokenRefreshSerializer(TokenRefreshSerializer):
+    refresh = None
+
+    def validate(self, attrs):
+        attrs['refresh'] = self.context['request'].COOKIES.get('refresh_token')
+        if attrs['refresh']:
+            return super().validate(attrs)
+        else:
+            raise InvalidToken(
+                'No valid token found in cookie \'refresh_token\''
+            )
+
+
+class CookieTokenRefreshView(TokenRefreshView):
+    serializer_class = CookieTokenRefreshSerializer
+
+    def finalize_response(self, request, response, *args, **kwargs):
+        if response.data.get('refresh'):
+            cookie_max_age = 3600 * 24 * 14  # 14 days
+            response.set_cookie(
+                'refresh_token', response.data['refresh'],
+                max_age=cookie_max_age, httponly=True
+            )
+            del response.data['refresh']
+        return super().finalize_response(request, response, *args, **kwargs)
 
 
 class TelegramAuthView(APIView):
@@ -17,3 +53,30 @@ class TelegramAuthView(APIView):
             bot_token=settings.TELEGRAM_BOT_TOKEN,
         ).execute()
         return Response(response_data, status=status.HTTP_200_OK)
+
+
+class TelegramAuthTestApi(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request: Request) -> Response:
+        user = User.objects.get(
+            telegram_id=request.query_params['telegram_id']
+        )
+        token = RefreshToken.for_user(user)
+        response = Response()
+        response.set_cookie(
+            'access_token',
+            str(token.access_token),
+            httponly=True,
+            samesite='Lax',
+            expires=token.access_token.get('exp'),
+        )
+        response.set_cookie(
+            'refresh_token',
+            str(token),
+            httponly=True,
+            samesite='Lax',
+            expires=token.get('exp'),
+        )
+        return response
