0fb9ff7b479d48a4f6342f749642cc860508ebb9
Eldos <eldos.baktybekov@gmail.com>
2025-04-11 16:38:12 +0600

    :sparkles: Add URL routing for exchange rates API

---
+++ exchange_rates/views.py
@@ -0,0 +1,12 @@
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+
+class ExchangeRateListApi(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request: Request) -> Response:
+        return Response()
