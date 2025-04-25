ffc4938acd52cfcc9fd7107ae92ef05e226b585f
Erkin <erkint@gmail.com>
2025-04-25 15:19:11 +0600

    :sparkles: Improve rate generation in factory by using Faker for random values

--- exchange_rates/tests/factories.py
+++ exchange_rates/tests/factories.py
@@ -2,6 +2,8 @@ import factory
 from django.utils.timezone import now
 from decimal import Decimal
 
+from faker.generator import random
+
 from exchange_rates.models import ExchangeRate
 
 
@@ -15,5 +17,6 @@ class ExchangeRateFactory(factory.django.DjangoModelFactory):
         ExchangeRate.CurrencyCode.RUB,
         ExchangeRate.CurrencyCode.KZT,
     ])
-    rate = factory.LazyFunction(lambda: Decimal('1.00') + Decimal(str(factory.random.randgen.random() * 100)).quantize(Decimal('0.01')))
+    # rate = factory.LazyFunction(lambda: Decimal('1.00') + Decimal(str(factory.random.randgen.random() * 100)).quantize(Decimal('0.01')))
+    rate = factory.LazyFunction(lambda: Decimal(f"{random.uniform(1, 100):.2f}"))
     updated_at = factory.LazyFunction(now)
